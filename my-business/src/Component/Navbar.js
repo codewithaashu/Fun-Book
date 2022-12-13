5c638070b2e9f0689c2e34861fa66fd3db4721af
codewithaashu <88059924+codewithaashu@users.noreply.github.com>
2022-12-14 01:40:54 +0530

    Add files via upload

---
+++ my-business/src/Component/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="bg-purple-200">
+        <nav>
+            <ul>
+                <li className=''>
+                    
+                </li>
+            </ul>
+        </nav>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
